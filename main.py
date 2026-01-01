f8168e2139fc38ad4ec7698eb9157b247617d1c1
dolevtamarie <dolevtamarie@gmail.com>
2026-01-01 20:01:39 +0200

    Implement edge detection on input image
    Load an image, apply edge detection, and save the result.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from image_utils import load_image, edge_detection
+from PIL import Image
 
+def main():
+    input_image_path = "dog_pic.jpg"
+    
+    img_array = load_image(input_image_path)
+    
+    edges = edge_detection(img_array)
+    
+    result_img = Image.fromarray(edges)
+    result_img.save("edge_detected_result.png")
+
+if __name__ == "__main__":
+    main()
