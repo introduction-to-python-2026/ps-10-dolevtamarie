11f7f29884d15c897fdbae68a3c579df5976d3b1
dolevtamarie <dolevtamarie@gmail.com>
2026-01-01 20:42:45 +0200

    Change image file from 'dog_pic.jpg' to 'cute_dog.jpg'

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 def main():
     # השתמשי בתמונה פשוטה, אולי אפילו תורידי תמונה אחרת מהאינטרנט
-    img = load_image("dog_pic.jpg") 
+    img = load_image("cute_dog.jpg") 
     edges = edge_detection(img)
     # שמירה בפורמט PNG (שלא מאבד נתונים כמו JPG)
     Image.fromarray(edges.astype('uint8')).save("edge_detected_result.png")
