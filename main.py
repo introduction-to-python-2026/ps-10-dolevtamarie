bde464fde1865a54784b6c8436fc423713c3ffff
dolevtamarie <dolevtamarie@gmail.com>
2026-01-01 20:33:10 +0200

    Refactor image loading and saving process
    Updated image processing to handle image format and type.

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ from image_utils import load_image, edge_detection
 from PIL import Image
 
 def main():
-    input_image_path = "dog_pic.jpg"
-    
-    img_array = load_image(input_image_path)
-    
-    edges = edge_detection(img_array)
-    
-    result_img = Image.fromarray(edges)
-    result_img.save("edge_detected_result.png")
+    # השתמשי בתמונה פשוטה, אולי אפילו תורידי תמונה אחרת מהאינטרנט
+    img = load_image("dog_pic.jpg") 
+    edges = edge_detection(img)
+    # שמירה בפורמט PNG (שלא מאבד נתונים כמו JPG)
+    Image.fromarray(edges.astype('uint8')).save("edge_detected_result.png")
 
 if __name__ == "__main__":
     main()
